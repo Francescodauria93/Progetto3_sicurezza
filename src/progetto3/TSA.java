4f7337f85fc3cac8d4170a94f20043aea50a66f1
fabriziodidomenico1 <33161590+fabriziodidomenico1@users.noreply.github.com>
2017-11-25 17:08:19 +0100

    Non toccate la Classe TSA e TEST ci sto lavorando io

--- src/progetto3/TSA.java
+++ src/progetto3/TSA.java
@@ -11,6 +11,10 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -18,7 +22,7 @@ import java.nio.file.Paths;
  */
 public class TSA {
 
-    public void merkelTree() throws IOException {
+    public void merkelTree() throws IOException, NoSuchAlgorithmException {
 
         Path currentRelativePath = Paths.get("src/progetto3");
         String s = currentRelativePath.toAbsolutePath().toString();
@@ -30,11 +34,23 @@ public class TSA {
             this.fillWaiting();
         }
 
-        byte[] readByte = null;
+        byte[] readByte = null; //byte letti temporanei
+        List<String> hID = new ArrayList<String>(); // lista di id
+        List<byte[]> hlist = new ArrayList<byte[]>();    //lista di h
+        List<byte[]> levelFour = new ArrayList<byte[]>();    // livello di 4 elementi
+        List<byte[]> levelTwo = new ArrayList<byte[]>();   //livello di 2 elementi
+        String hashRoot ; // root Hash
+        
+       // qui costruisco hlist e hID
         for (File child : directoryListing) {
             readByte = fileUtility.loadFile(child.toString()); // leggo il file
-            
+            hlist.add(Arrays.copyOfRange(readByte, 0, 32));
+            hID.add(Arrays.copyOfRange(readByte, 32 + 1, readByte.length).toString());
         }
+        
+        MessageDigest sha = MessageDigest.getInstance("SHA-256"); //creo una istanza di SHA
+        /*sha.update(fileReaded);
+        byte[] hashFileReaded = sha.digest();*/
 
     }
 
@@ -42,16 +58,5 @@ public class TSA {
 
     }
 
-    public static void readDoc() {
-        //questo è quello che leggi da file
-        byte[] fileReaded = null;
-        byte[] docFile = new byte[32];
-        byte[] userID = null;
-        //carico l'hash del file nel suo array di byte 
-
-        docFile = Arrays.copyOfRange(fileReaded, 0, 32);
-        userID = Arrays.copyOfRange(fileReaded, 32 + 1, fileReaded.length);
-
-    }
-
+  
 }
